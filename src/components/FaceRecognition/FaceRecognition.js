d2264e2b488f2f2ff54bb5c850889597aea14e58
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2022-01-15 22:55:19 -0300

    feat: detect more than one face

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,25 @@
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ box, imageUrl }) => {
+const FaceRecognition = ({ boxes, imageUrl }) => {
   return (
     <div className='image-container'>
       <div className='image-wrapper'>
         <img id='inputimage' alt='' src={imageUrl} className='image' />
-        <div className='bounding-box' style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
+        <div>
+          {
+            boxes.map((box, index) => (
+              <div key={index}
+                className='bounding-box' 
+                style={{ 
+                  top: box.topRow, 
+                  right: box.rightCol, 
+                  bottom: box.bottomRow, 
+                  left: box.leftCol 
+                }}>
+              </div>
+            ))
+          }
+        </div>
       </div>
     </div>
   );
