49713db9ba8c81508dfb3fcf8afe22a4ab5ab30e
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2022-01-15 23:10:52 -0300

    refactor: create CustomParticles

---
+++ src/components/CustomParticles/CustomParticles.js
@@ -0,0 +1,51 @@
+import Particles from 'react-tsparticles';
+import { loadLinksPreset } from "tsparticles-preset-links";
+
+const particlesOptions = {
+	preset: "links",
+	particles: {
+		number: {
+			value: 60,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		},
+		line_linked: {
+			opacity: 0.4,
+			color: {
+				value: "#000"
+			},
+			shadow: {
+				enable: true,
+				color: '#141414',
+				blur: 5
+			}
+		},
+		color: {
+			value: "#000"
+		},
+		stroke: {
+			color: "#000"
+		},
+		size: {
+			value: 2
+		}
+	},
+	background: {
+		color: "#FFF"
+	}
+}
+
+function customInit(main) {
+  loadLinksPreset(main);
+}
+
+const CustomParticles = () => {
+
+  return (
+    <Particles className='particles' id="tsparticles" options={particlesOptions} init={customInit}/>
+  );
+};
+
+export default CustomParticles;
